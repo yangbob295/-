0c883780dabe173db1a8b055ef8779883bd3d42d
yangbob295 <yangbob295@gmail.com>
2025-05-13 14:38:34 +0800

    加了User，准备写SaveBtn，忘了整曹操

--- project2025/project/src/view/game/GameFrame.java
+++ project2025/project/src/view/game/GameFrame.java
@@ -2,6 +2,7 @@ package view.game;
 
 import controller.GameController;
 import model.MapModel;
+import user.User;
 import view.FrameUtil;
 
 import javax.swing.*;
@@ -12,9 +13,11 @@ public class GameFrame extends JFrame {
     private GameController controller;
     private JButton restartBtn;
     private JButton loadBtn;
+    private JButton saveBtn;
 
     private JLabel stepLabel;
     private GamePanel gamePanel;
+    private User user;
 
     public GameFrame(int width, int height, MapModel mapModel) {
         this.setTitle("2025 CS109 Project Demo");
@@ -22,11 +25,13 @@ public class GameFrame extends JFrame {
         this.setSize(width, height);
         gamePanel = new GamePanel(mapModel);
         gamePanel.setLocation(30, height / 2 - gamePanel.getHeight() / 2);
+        //此处添加了location修改，初始为30
         this.add(gamePanel);
         this.controller = new GameController(gamePanel, mapModel);
 
         this.restartBtn = FrameUtil.createButton(this, "Restart", new Point(gamePanel.getWidth() + 80, 120), 80, 50);
         this.loadBtn = FrameUtil.createButton(this, "Load", new Point(gamePanel.getWidth() + 80, 210), 80, 50);
+        this.saveBtn = FrameUtil.createButton(this, "Sava", new Point(gamePanel.getWidth() + 80, 300), 80, 50);
         this.stepLabel = FrameUtil.createJLabel(this, "Start", new Font("serif", Font.ITALIC, 22), new Point(gamePanel.getWidth() + 80, 70), 180, 50);
         gamePanel.setStepLabel(stepLabel);
 
@@ -40,6 +45,15 @@ public class GameFrame extends JFrame {
             gamePanel.requestFocusInWindow();//enable key listener
         });
         //todo: add other button here
+
+
+        this.saveBtn.addActionListener(e -> {
+//            controller.saveGame(user);savegame还没写
+//待完成：-------------------------------------------------------------------
+            gamePanel.requestFocusInWindow();//enable key listener
+        });
+
+
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
